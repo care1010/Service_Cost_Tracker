49218f7037371f7e360b85f8adcc5e680e4ea534
care <care.ni_india@nokia.com>
2026-05-26 21:28:15 +0530

    success

--- server/config/db.js
+++ server/config/db.js
@@ -11,8 +11,6 @@ const pool = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    // connectTimeout: 60000, // 60 seconds
-    // acquireTimeout: 60000  // 60 seconds
 });
 
 module.exports = pool.promise();
